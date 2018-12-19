4b7dade9e86cacdfda036756c62f5b4532733e1d
eric.njanga <eric.njanga@MLA0RLI095.local>
2018-12-19 16:24:52 -0500

    Triggers sound play on click

--- src/components/WordsAndSound.js
+++ src/components/WordsAndSound.js
@@ -11,36 +11,60 @@ import Button from '@material-ui/core/Button'
 import Tooltip from '@material-ui/core/Tooltip'
 import style from './../styles/StyledWordsAndSound'
 
-const WordsAndSound = () => {
 
-  const { Container } = style
-  // font-size: 0.5rem;
-  return (
-    <Container>
-      <Fab aria-label="Delete" className="WordsAndSound__btn">
-        <IconVoice
-          fontSize="small"
-          className="WordsAndSound__icon"
-        />
-      </Fab> 
+class WordsAndSound extends React.Component {
 
-      <div className="WordsAndSound__phrase">
-        <Tooltip disableFocusListener disableTouchListener title="Quel">
-          <span>Quel</span>
-        </Tooltip>
-        <Tooltip disableFocusListener disableTouchListener title="est">
-          <span>est</span>
-        </Tooltip>
-        <Tooltip disableFocusListener disableTouchListener title="ton">
-          <span>ton</span>
-        </Tooltip>
-        <Tooltip disableFocusListener disableTouchListener title="nom">
-          <span>nom</span>
-        </Tooltip>
-        ?
-      </div>
-    </Container>
-  )
+  constructor(props) {
+    super(props)
+    this.audio = React.createRef()
+    this.state = {
+      audio: { active: false, url: 'https://raw.githubusercontent.com/scottschiller/SoundManager2/master/demo/_mp3/going_outside.mp3' }
+    }
+  }
+
+
+  render () {
+    const { Container } = style
+    const { audio } = this.state
+    
+    return (
+      <Container>
+        <Fab
+          aria-label="Delete"
+          className="WordsAndSound__btn"
+          onClick={
+            () => {
+              this.audio.current.play()
+            }
+          }
+        >
+          <IconVoice
+            fontSize="small"
+            className="WordsAndSound__icon"
+          />
+        </Fab> 
+        
+        <audio ref={this.audio} src={audio.url} autoPlay={false} />
+
+        <div className="WordsAndSound__phrase">
+          <Tooltip disableFocusListener disableTouchListener title="Quel">
+            <span>Quel</span>
+          </Tooltip>
+          <Tooltip disableFocusListener disableTouchListener title="est">
+            <span>est</span>
+          </Tooltip>
+          <Tooltip disableFocusListener disableTouchListener title="ton">
+            <span>ton</span>
+          </Tooltip>
+          <Tooltip disableFocusListener disableTouchListener title="nom">
+            <span>nom</span>
+          </Tooltip>
+          ?
+        </div>
+      </Container>
+    )
+
+  }
 }
  
 export default WordsAndSound
